3848f14b3639af491ed259ea263937d909dbd21f
Gabriel Lopez <gabelopez84@gmail.com>
2021-03-30 15:34:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function scuberGreetingForFeet(199){
+function scuberGreetingForFeet(<=199){
   if (199) {
-    //  return the correct greeting based on the distance the passenger desires to travel.
-  } else function (2000) { 'charges 30 dollars for a distance over 2000 feet'
+    //  this one is one me
+  } else function (>199) { 'charges 30 dollars for a distance over 2000 feet' } 
     // block of code to be executed if the condition is false
   }
 }
@@ -15,14 +15,5 @@ function ternaryCheckCity(){
   // expected output: "This one is on me!"
 
 function switchOnCharmFromTip(){
-  const expr = '199';
-  switch (expr) {
-    case 'free ride':
-      console.log('Oranges are $0.59 a pound.');
 
-      // expected output: ""This one is on me!""
-      break;
-    default:
-      console.log(`Sorry, we are out of ${expr}.`);
-  }
 }
