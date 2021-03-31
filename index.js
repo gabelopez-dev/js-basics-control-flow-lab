83422a252f222098a44255bda5ba09d36aa11164
Gabriel Lopez <gabelopez84@gmail.com>
2021-03-31 14:31:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function scuberGreetingForFeet(feet){
   if (feet <=399) { 
     return 'This one is on me!';} 
+    // gives customers a free sample if the ride is less than or equal to 400 feet
   else if (feet >400 && feet < 2500) {
     return 'I will gladly take your thirty bucks.';
     // charges 30 dollars for a distance over 2000 feet'
   }
+  else if (feet > 2500) {
+    return 'No can do.';
+    // does not allow rides over 2500 feet
 } 
-
-  function ternaryCheckCity(){
-    return (feet);
-    console.log(getFee(true));
-  // expected output: 
-
+}
+  function ternaryCheckCity(city){
+    let city = NYC;
+ 
+    city = NYC ? (city = true) : (city= false);
+    // => true
+   return "Ok, sounds good."
+  }
 function switchOnCharmFromTip(){
 
 }
