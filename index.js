85348e9a380cc2a2402c627bfac3949aaa3a92bb
Gabriel Lopez <gabelopez84@gmail.com>
2021-03-31 12:29:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function scuberGreetingForFeet(feet){
   if (feet <=399) { 
     return 'This one is on me!';} 
-  else if (feet >400 && feet < 2500); {
+  else if (feet >400 && feet < 2500) {
     return 'I will gladly take your thirty bucks.';
     // charges 30 dollars for a distance over 2000 feet'
   }
